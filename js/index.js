bcdb7b1c35db53b98b630deb92eaff9692815fc5
adgianv <a.digianvito@hotmail.it>
2024-04-19 16:37:22 +0200

    fonts modified

--- js/index.js
+++ js/index.js
@@ -96,8 +96,8 @@ gsap.to('#portfolio',0,{display:"none"});
    gsap.to('#breaker',0,{display:"block"});
    gsap.to('#breaker-two',0,{display:"block",delay:.1});
 gsap.to('#contact',0,{display:"none"});
-   gsap.to('#breaker',0,{display:"none",delay:2});
-   gsap.to('#breaker-two',0,{display:"none",delay:2});
+   gsap.to('#breaker',0,{display:"none",delay:.7});
+   gsap.to('#breaker-two',0,{display:"none",delay:.7});
    gsap.to('#about',0,{display:"block",delay:.7});
    gsap.to('#navigation-content',0,{display:'flex',delay:2});
  })
